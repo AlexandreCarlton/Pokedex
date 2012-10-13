86f06b463d6815cb4bf2b51dd6f80ba7e09aa433
Alexandre Carlton <acar7163@uni.sydney.edu.au>
2012-10-13 14:17:30 +1100

    Redefined structure of project; now scripts are located in scripts folder, media contains sprites and cries of pokemon. Also completed black_and_white_animated() for PokemonMediaScraper, although when displaying it a way will be needed to pad each one out such that they all have the same dimensions when viewed

--- scripts/PokemonMediaScraper.py
+++ scripts/PokemonMediaScraper.py
@@ -1,4 +1,6 @@
-from urllib2 import urlopen, HTTPError, URLError
+from urllib2 import urlopen, HTTPError
+from BeautifulSoup import BeautifulSoup
+
 
 class PokemonMediaScraper(object):
     '''Scrapes sprites and cries from the web and saves them to
@@ -14,27 +16,41 @@ class PokemonMediaScraper(object):
             with open(filename, 'w') as file:
                 file.write(url_file)
         except HTTPError, e:
-            print 'Unable to retrieve', url, 'due to HTTP Error', e.code 
-        except URLError, e:
-            print 'Unable to retrive', url, 'due to URL Error', e.code
+            # We've found a Pokemon without female differences.
+            pass
 
     def cry(self):
+        '''Saves Pokemon's cry in an .ogg file to ../media/cries/
+        Uses Veekun'''
+
         cry_url = 'http://veekun.com/dex/media/pokemon/cries/%d.ogg' % self.num
         cry_name = '../media/cries/%03d.ogg' % self.num
         self._save_file(cry_url, cry_name)
 
+
     def black_and_white_animated(self):
-        '''Saves the B&W animated sprites 
-        both male and (if exists) female'''
-        # TODO
-
-    def black_and_white_shiny_animated(self):
-        '''Saves the B&W animated shiny sprites
-        both male and (if exists) female'''
-        # TODO
-
+        '''Saves the B&W animated sprites
+            both normal and shiny
+            both male and (if exists) female 
+        to ../media/sprites/black_white/{normal,shiny}
+        Female pokemon have a 'f' suffix
+        Uses Pokecheck'''
+        url_genders = ['', 'f']
+        url_colours = ['i', 's']
+        
+        base_url = 'http://sprites.pokecheck.org/%s/%03d%s.gif'
+        base_file_name = '../media/sprites/black_white/%s/%03d%s.gif' # FIXME
+        
+        for g in url_genders:
+            for c in url_colours:
+                file_name = base_file_name % ('normal' if c == 'i' else 'shiny', self.num, g)
+                url = base_url % (c, self.num, g)
+                
+                self._save_file(url, file_name)
 
 if __name__ == '__main__':
-    bulbasaur = PokemonMediaScraper(2)
+    bulbasaur = PokemonMediaScraper(1)
     bulbasaur.cry()
-
+    rattata = PokemonMediaScraper(19)
+    rattata.black_and_white_animated()
+    bulbasaur.black_and_white_animated()
